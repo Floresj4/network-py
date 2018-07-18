e2a118ef764848bc12ef2050804a183f72f8f377
Jason <floresj4@gmail.com>
2018-07-17 22:16:41 -0400

    creating additional functions and fiddling around

--- ip-addressing.py
+++ ip-addressing.py
@@ -6,16 +6,32 @@ def addressToBinary(addr):
     #handle the values as integers
     blocks = [int(x) for x in addr.split('.')]
 
-    for dgt in blocks:
-        bits = []
-        for power in range(7, -1, -1):
-            block_value = math.pow(2, power)
-            if (block_value <= dgt):
-                bits.append(1)
-                dgt -= block_value
-            else: bits.append(0)
-        print(bits)
+    position = 0
+    for block in blocks:
+        bits = blockToBits(block)
+        blocks[position] = bits
+        position += 1
 
     return blocks
 
+def addressToBinaryStr(addr):
+    return ''.join(addressToBinary(addr))
+
+def blockToBits(block):
+    bits = []
+
+    for power in range(7, -1, -1):
+        block_value = math.pow(2, power)
+
+        if (block_value <= block):
+            bits.append(1)
+            block -= block_value
+        else: bits.append(0)
+
+    return bits
+
+def blockToBitsStr(block):
+    return ''.join(str(b) for b in blockToBits(block))
+
 print(addressToBinary(ipaddress))
+print(blockToBitsStr(255))
